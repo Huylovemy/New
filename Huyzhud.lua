efb6d617bad1ef83b84ea18a3e8a65665da00b94
Quang Huy <249802984+Huylovemy@users.noreply.github.com>
2026-01-19 18:13:47 +0700

    Update Huyzhud.lua

--- Huyzhud.lua
+++ Huyzhud.lua
@@ -23,7 +23,7 @@ local Bearlib = {
             ["Color Stroke"] = Color3.fromRGB(255,255,255), 
             
             -- Màu các nút bấm và icon (để màu trắng hoặc xanh neon cho dễ nhìn)
-            ["Color Theme"] = Color3.fromRGB(0,0,0), 
+            ["Color Theme"] = Color3.fromRGB(255,255,255), 
             
             ["Color Text"] = Color3.fromRGB(0,0,0),
             ["Color Dark Text"] = Color3.fromRGB(0,0,0)
